c585c3dfc02c47bb548692b914a4608a5cb2952a
matialm <matias_almad@hotmail.com>
2017-11-25 00:31:24 -0300

    cluster para el server
    Se agrego cluster para el server.js, el mismo se levanta con un worker y al morir el mismo se levanta otro.

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 var express = require('express'),
+	cluster = require('cluster'),
     app = express(),
     port = process.env.PORT || 3000,
     bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+if (cluster.isMaster) {
+	cluster.fork();
+	
+	cluster.on('exit', (worker, code, signal) => {
+		console.log(`worker ${worker.process.pid} died`);
+		cluster.fork();
+	});
+}
+else {
+	app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(bodyParser.json());
 
 
-var routes = require('./api/routes/routes');
-routes(app);
+	var routes = require('./api/routes/routes');
+	routes(app);
 
 
-app.listen(port);
+	app.listen(port);
 
 
-console.log('todo list RESTful API server started on: ' + port);
+	console.log('todo list RESTful API server started on: ' + port);
 
-module.exports = app;
+	module.exports = app;
+	console.log(`Worker ${process.pid} started`);
+}
